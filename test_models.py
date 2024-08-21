82ca864e4806dbea6a6a2094b55b2e2718330705
LghtsNtloko <ST10125711@rcconnect.edu.za>
2024-08-21 15:32:47 +0000

    unittest module

---
+++ test_models.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+import unittest
+
+
+class TestModels(unittest.TestCase):
+    def test_something(self):
+        self.assertEqual(1 + 1, 2)
+
+if __name__ == '__main__':
+    unittest.main()
